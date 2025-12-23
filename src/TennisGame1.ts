7262d039f81ce49b6112ab67917343c1cab1547b
Abrahim Saad <abrahimsaad271@gmail.com>
2025-12-23 18:17:33 +0300

    style: move calculateScore method to the top

--- src/TennisGame1.ts
+++ src/TennisGame1.ts
@@ -24,36 +24,6 @@ export class TennisGame1 implements TennisGame {
   }
 
 
-  calculateScoreWhenBothScoresAreEqual(m_score: number, score: string): string {
-    switch (m_score) {
-      case 0:
-        score = 'Love-All';
-        break;
-      case 1:
-        score = 'Fifteen-All';
-        break;
-      case 2:
-        score = 'Thirty-All';
-        break;
-      default:
-        score = 'Deuce';
-        break;
-
-    }
-    return score;
-  }
-
-
-  calculateScoreWhenOneScoreIsAtLeast4(m_score1: number, m_score2: number, score: string): string {
-    const minusResult: number = m_score1 - m_score2;
-    if (minusResult === 1) score = 'Advantage player1';
-    else if (minusResult === -1) score = 'Advantage player2';
-    else if (minusResult >= 2) score = 'Win for player1';
-    else score = 'Win for player2';
-    return score;
-  }
-
-
   calculateScore(m_score1: number, m_score2: number): string {
     let score: string = '';
     let tempScore: number = 0;
@@ -83,4 +53,35 @@ export class TennisGame1 implements TennisGame {
 
     return score;
   }
+
+
+  calculateScoreWhenBothScoresAreEqual(m_score: number, score: string): string {
+    switch (m_score) {
+      case 0:
+        score = 'Love-All';
+        break;
+      case 1:
+        score = 'Fifteen-All';
+        break;
+      case 2:
+        score = 'Thirty-All';
+        break;
+      default:
+        score = 'Deuce';
+        break;
+
+    }
+    return score;
+  }
+
+
+  calculateScoreWhenOneScoreIsAtLeast4(m_score1: number, m_score2: number, score: string): string {
+    const minusResult: number = m_score1 - m_score2;
+    if (minusResult === 1) score = 'Advantage player1';
+    else if (minusResult === -1) score = 'Advantage player2';
+    else if (minusResult >= 2) score = 'Win for player1';
+    else score = 'Win for player2';
+    return score;
+  }
+
 }
